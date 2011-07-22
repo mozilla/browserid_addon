28da608948ee7cacd7dbf5718464504d9abc702d
Shane Tomlinson <stomlinson@mozilla.com>
2011-07-22 16:54:22 -0700

    window manager keeps track of current session.
    That's it, and it keeps the "current" session model up to date with whatever the current session is.

--- addons/sessions/lib/window_manager.js
+++ addons/sessions/lib/window_manager.js
@@ -1,66 +1,28 @@
-const {WindowTracker} = require("window-utils");
-const {SessionDisplay} = require("./session_display");
-const {Helpers} = require("./helpers");
-const self = require("self");
 
-const logins = [];
-logins.login = function(host) {
-    this.forEach(function(login) {
-        login.login(host);
-    });
+let WindowManager = function(config) {
+    this.session = config.session;
 };
 
-logins.loggedIn = function(username, host) {
-    this.forEach(function(login) {
-        login.loggedIn(username, host);
-    });
-};
+WindowManager.prototype = {
+    setSession: function(session) {
+        session.getFields().forEach(function(field) {
+            this.session[field] = session[field];
+        }, this);
 
-logins.none = function() {
-    this.forEach(function(login) {
-        login.none();
-    });
-};
+        if(this.currentSession) {
+            this.currentSession.removeListener("set", this.currentOnSet);
+        }
 
-let login, logout, sessions;
-var delegate = {
-    onTrack: function(window) {
-         console.log("window track");
-         let sessionDisplay = SessionDisplay({
-             window: window,
-             document: window.document
-         });
-         sessionDisplay.none();
-         sessionDisplay.on("login", function() {
-            login();   
-         });
-         sessionDisplay.on("userinfo", function() {
-             sessions();
-         });
-         logins.push(sessionDisplay);
-
-         let uri = self.data.url("styles/identity-session.css");
-         Helpers.chrome.loadStylesheet(uri, window.document);
-    },
-
-    onUntrack: function(window) {
-         console.log("Untracking a window: " + window.location);
+        this.currentSession = session;
+        this.currentOnSet = onSet.bind(this);
+        session.on("set", this.currentOnSet);
     }
-
 };
 
-var tracker = new WindowTracker(delegate);
-
-exports.WindowManager = function(loginCallback, logoutCallback, sessionCallback) {
-    login = loginCallback;
-    logout = logoutCallback;
-    sessions = sessionCallback;
-
-    return {
-        none: logins.none.bind(logins),
-        login: logins.login.bind(logins),
-        loggedIn: logins.loggedIn.bind(logins)
-    };
-};
+function onSet(info) {
+    this.session[info.name] = info.value;
+}
 
 
+
+exports.WindowManager = WindowManager;
--- addons/sessions/test/test-window_manager.js
+++ addons/sessions/test/test-window_manager.js
@@ -1,2 +1,46 @@
 const {WindowManager} = require("window_manager");
+const {Session} = require("session");
 
+let wm, session;
+
+exports.setup = function() {
+    session = new Session();
+    wm = new WindowManager({
+        session: session
+    });
+};
+
+exports['do we have a window manager'] = function(test) {
+    test.assertEqual(!!wm, true, 'we have a window manager');
+};
+
+exports['setSession updates current session'] = function(test) {
+    let newSession = new Session();
+    newSession.email = "stomlinson@mozilla.com";
+
+    wm.setSession(newSession);
+
+    test.assertEqual(session.email, "stomlinson@mozilla.com", 'setting the session updates the original session');
+};
+
+exports['updating session after it is set current updates the currentSession'] = function(test) {
+    let newSession = new Session();
+
+    wm.setSession(newSession);
+    newSession.email = "stomlinson@mozilla.com";
+
+    test.assertEqual(session.email, "stomlinson@mozilla.com", 'setting the session updates the original session');
+};
+
+
+
+exports['multiple sessions works alright'] = function(test) {
+    let firstSession = new Session();
+    let secondSession = new Session();
+
+    wm.setSession(firstSession);
+    wm.setSession(secondSession);
+
+    firstSession.email = "stomlinson@mozilla.com";
+    test.assertNotEqual(session.email, "stomlinson@mozilla.com", "setting data on a past session");
+};
