4b88ae5c29fb6ddb12e04d8209f047b288faa41d
Shane Tomlinson <set117@yahoo.com>
2011-07-24 03:03:17 -0700

    When going to new page, get ready to clear old results.

--- addons/sessions/lib/main.js
+++ addons/sessions/lib/main.js
@@ -4,7 +4,6 @@ const {Widget} = require("widget");
 const {PageMod} = require("page-mod");
 const {Fakers} = require("./fakers");
 const {Helpers} = require("./helpers");
-const timers = require("timers");
 const {WindowManager} = require("window_manager");
 const {TabManager} = require("tab_manager");
 Fakers();
@@ -21,10 +20,8 @@ let pageMod = PageMod({
     onAttach: function(worker) {
         Helpers.workers.prepare(worker);
 
-        console.log("we have a worker");
-        console.log("worker.tab.window.expando in pagemod: " + worker.tab.window.expando);
         worker.port.on("sessions.set", onSessionSet.bind(worker));
-        worker.port.on("sessions.opentab", onSessionTabOpen.bind(worker, worker.tab));
+        worker.port.on("sessions.opentab", onSessionTabOpen.bind(worker));
     }
 });
 
@@ -35,10 +32,8 @@ function onSessionSet(data) {
 }
 
 
-function onSessionTabOpen(tab) {
-    console.log("session tab open");
-
-    console.log("typeof tab.window: " + typeof tab.window);
+function onSessionTabOpen() {
+    tabManager.sessionReset(this.tab);
 };
 
 
--- addons/sessions/lib/tab_manager.js
+++ addons/sessions/lib/tab_manager.js
@@ -3,7 +3,7 @@
 const {Session} = require("session");
 const {Helpers} = require("helpers");
 const tabs = require("tabs");
-
+const timers = require("timers");
 
 let TabManager = function() {
     for each(let tab in tabs) {
@@ -11,32 +11,40 @@ let TabManager = function() {
     }
     tabs.on("open", createTabSession);
     tabs.on("activate", setActiveTab);
-    tabs.on("ready", onTabReady);
+    tabs.on("ready", onTabReady.bind(this));
 };
 TabManager.prototype = {
     constructor: TabManager,
     sessionsUpdate: function(tab, data) {
-      console.log("updating sessions: " + data.sessions.length);
        let session = tab.session;
        session.sessions = data.sessions;
+        this._resetSession = false;
+    },
+    sessionReset: function(tab) {
+        console.log("session reset");
+        this._resetSession = true;
     }
 };
 
 function createTabSession(tab) {
     if(!tab.session) {
         tab.session = Session();
+        setActiveTab(tab);
     }
 }
 
 function setActiveTab(tab) {
   console.log("new tab made active");
     let windowSessions = tab.window.session;
-//    Helpers.toConsole(tab.session);
-  //  console.log("session length: " + tab.session.sessions.length);
     windowSessions.session = tab.session;
 }
 
-function onTabReady(){}
+function onTabReady(tab){
+  if(this._resetSession) {
+      console.log("resetting sessions");
+      tab.session.sessions = undefined;
+  }
+}
 
 exports.TabManager = TabManager;
 /*
