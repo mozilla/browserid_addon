3278f65a32880a74edb133a1bfa65603dedb0368
Shane Tomlinson <set117@yahoo.com>
2011-07-24 00:13:49 -0700

    Updating to hold all of the view logic.
    Now that session model is updated to have all of the sessions and not just the current one, we can move all of the necessary logic here and keep it from polluting outside this realm.

--- addons/sessions/lib/session_display.js
+++ addons/sessions/lib/session_display.js
@@ -42,32 +42,6 @@ const SessionDisplay = EventEmitter.compose({
 
 exports.SessionDisplay = SessionDisplay;
 
-const NODE_SPECIAL = ['parentNode'];
-function createNode(document, nodeName, attribs) {
-     let node = document.createElementNS(XUL_NS, nodeName);
-
-     node.show = function() {
-        node.collapsed = node.hidden = false;
-     };
-
-     node.hide = function() {
-        node.collapsed = node.hidden = true;
-     };
-
-     for(let attrib in attribs) {
-         let val = attribs[attrib];
-         if(NODE_SPECIAL.indexOf(attrib) === -1) {
-            node.setAttribute(attrib, val);
-         }
-         else if(attrib === 'parentNode') {
-             val.insertBefore(node, null);
-         }
-     }
-
-     return node;
-}
-
-
 function createUI(document) {
     this.box = createNode(document, "box", {
         id: "identity-session-box"
@@ -94,18 +68,72 @@ function createUI(document) {
 }
 
 function attachSessionEvents(session) {
-    session.on("set:email", onSetEmail.bind(this));
-    session.on("set:status", onSetStatus.bind(this));
+    session.on("set:sessions", onSetSessions.bind(this));
 }
 
-function onSetEmail(value) {
-    this.userInfo.setAttribute("value", value);
-}
+function onSetSessions(sessions) {
+    var status = "none";
+    if(sessions) {
+        let email = getActiveEmail(sessions);
 
-function onSetStatus(value) {
-    if(STATUS_SHOW.indexOf(value) > -1) {
-        this.show();
-    } else {
-        this.hide();
+        status = "login";
+        if(email) {
+            this.userInfo.setAttribute("value", email);
+            status = "loggedin";
+        }
     }
+
+    setStatus.call(this, status);
 }
+
+function getActiveEmail(sessions) {
+    let email = sessions[0] && sessions[0].email;
+    return email;
+}
+
+function setStatus(status) {
+    switch(status) {
+        case "login":
+            this.signIn.show();
+            this.userInfo.hide();
+            this.show();
+            break;
+        case "loggedin":
+            this.signIn.hide();
+            this.userInfo.show();
+            this.show();
+            break;
+        default:
+            this.hide();
+            break;
+    } 
+}
+
+
+const NODE_SPECIAL = ['parentNode'];
+function createNode(document, nodeName, attribs) {
+     let node = document.createElementNS(XUL_NS, nodeName);
+
+     node.show = function() {
+        node.collapsed = node.hidden = false;
+     };
+
+     node.hide = function() {
+        node.collapsed = node.hidden = true;
+     };
+
+     for(let attrib in attribs) {
+         let val = attribs[attrib];
+         if(NODE_SPECIAL.indexOf(attrib) === -1) {
+            node.setAttribute(attrib, val);
+         }
+         else if(attrib === 'parentNode') {
+             val.insertBefore(node, null);
+         }
+     }
+
+     return node;
+}
+
+
+
--- addons/sessions/test/test-session_display.js
+++ addons/sessions/test/test-session_display.js
@@ -16,50 +16,43 @@ exports.setup = function() {
     widget.hide();
 };
 
-exports['test creatable'] = function(test) {
-    test.assertNotUndefined(widget, 'widget has been created');
+exports["test creatable"] = function(test) {
+    test.assertNotUndefined(widget, "widget has been created");
 };
 
-exports['adds elements'] = function(test) {
-    let box = Helpers.chrome.getElementById('identity-session-box');
-    test.assertEqual(true, !!box, 'box has been added');
+exports["adds elements"] = function(test) {
+    let box = Helpers.chrome.getElementById("identity-session-box");
+    test.assertEqual(true, !!box, "box has been added");
 };
 
-exports['login emitted when signIn button pressed'] = function(test) {
+exports["login emitted when signIn button pressed"] = function(test) {
     let success = false;
     widget.on("login", function() {
         success = true;
     });
 
-    let signIn = Helpers.chrome.getElementById('identity-session-signin');
+    let signIn = Helpers.chrome.getElementById("identity-session-signin");
     let evt = Helpers.chrome.simulateDOMEvent(signIn, "MouseEvents", "click");
 
     test.assertEqual(success, true, "login event fired on click");
 };
 
-exports['userinfo emitted when userinfo button pressed'] = function(test) {
+exports["userinfo emitted when userinfo button pressed"] = function(test) {
     let success = false;
     widget.on("userinfo", function() {
         success = true;
     });
 
-    let signIn = Helpers.chrome.getElementById('identity-session-userinfo');
+    let signIn = Helpers.chrome.getElementById("identity-session-userinfo");
     let evt = Helpers.chrome.simulateDOMEvent(signIn, "MouseEvents", "click");
 
     test.assertEqual(success, true, "userinfo event fired on click");
 };
 
 
-exports['changing the model email changes userinfo'] = function(test) {
-    session.email = "shane";    
-    let box = Helpers.chrome.getElementById("identity-session-userinfo");
-
-    test.assertEqual(box.value, "shane", "update email, causes userinfo to update");
-};
-
-exports['show shows the box'] = function(test) {
+exports["show shows the box"] = function(test) {
     let success = false;
-    widget.on('show', function() {
+    widget.on("show", function() {
         success = true;
     });
     widget.show();
@@ -71,9 +64,9 @@ exports['show shows the box'] = function(test) {
 
 
 
-exports['hide hides the box'] = function(test) {
+exports["hide hides the box"] = function(test) {
     let success = false;
-    widget.on('hide', function() {
+    widget.on("hide", function() {
         success = true;
     });
     widget.hide();
@@ -84,38 +77,41 @@ exports['hide hides the box'] = function(test) {
 };
 
 
-
-exports['setting the status to login shows box'] = function(test) {
+exports["setting no sessions hides the box"] = function(test) {
     let success = false;
-    widget.on('show', function() {
+    widget.on("hide", function() {
         success = true;
     });
 
-    session.status = 'login';
-    test.assertEqual(success, true, 'we show the box when setting status to login');
+    session.sessions = undefined;
+    test.assertStrictEqual(success, true, "Setting no sessions hides the box");
+
 };
 
-exports['setting the status to none hides box'] = function(test) {
-    let show = false, hide = false;
-    widget.on('show', function() {
-        show = true;
-    });
-    widget.on('hide', function() {
-        hide = true;
-    });
-
-    session.status = 'login';
-    session.status = 'none';
-    test.assertEqual(show && hide, true, 'we hide the box when setting status to none');
-};
-
-exports['setting the status to loggedin shows box'] = function(test) {
+exports["setting a the sessions to an empty array shows the box"] = function(test) {
     let success = false;
-    widget.on('show', function() {
+    widget.on("show", function() {
         success = true;
     });
 
-    session.status = 'loggedin';
-    test.assertEqual(success, true, 'we show the box when setting status to loggedin');
+    session.sessions = [];
+    test.assertStrictEqual(success, true, "Setting a session shows the box");
 };
+
+exports["setting a session shows the box, updates userinfo"] = function(test) {
+    let success = false;
+    widget.on("show", function() {
+        success = true;
+    });
+
+    session.sessions = [{
+        email: "shane"
+    }];
+    test.assertStrictEqual(success, true, "Setting a session shows the box");
+
+    let box = Helpers.chrome.getElementById("identity-session-userinfo");
+
+    test.assertEqual(box.value, "shane", "update email, causes userinfo to update");
+};
+
 
