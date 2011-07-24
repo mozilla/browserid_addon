789db2db83ca263c4c7fe85a2665c4cbe5b40cf7
Shane Tomlinson <set117@yahoo.com>
2011-07-23 18:41:33 -0700

    starting to simplify the onSessionSet

--- addons/sessions/lib/main.js
+++ addons/sessions/lib/main.js
@@ -1,3 +1,4 @@
+"use strict";
 const {data} = require("self");
 const {Panel} = require("panel");
 const {Widget} = require("widget");
@@ -6,7 +7,8 @@ const {Fakers} = require("./fakers");
 const tabs = require("tabs");
 const {Helpers} = require("./helpers");
 const timers = require("timers");
-
+const {WindowManager} = require("window_manager");
+const {TabManager} = require("tab_manager");
 const {CookieMonster} = require("./cookie_monster");
 
 var cookieMonster = new CookieMonster();
@@ -27,6 +29,8 @@ panel.port.on("sessions.logout", function() {
 
 Fakers();
 
+let windowManager = new WindowManager();
+let tabManager = new TabManager();
 
 var currWorker;
 
@@ -38,7 +42,7 @@ var pageMod = PageMod({
         Helpers.workers.prepare(worker);
 
         console.log("we have a worker");
-
+        console.log("worker.tab.window.expando in pagemod: " + worker.tab.window.expando);
         worker.port.on("sessions.set", onSessionSet.bind(worker));
         worker.port.on("sessions.opentab", onSessionTabOpen.bind(worker, worker.tab));
     }
@@ -47,57 +51,26 @@ var pageMod = PageMod({
 
 
 function onSessionSet(data) {
-    console.log("session set");
     currWorker = this;
-    var tab = this.tab;
-    var sessions = data.sessions;
-    var host = data.host;
+    let tab = this.tab;
+    let sessions = data.sessions;
+    
+    let status = tabManager.sessionsUpdate(tab, sessions);
 
-    if("undefined" === typeof sessions) {
-        loginWidget.none();
-        console.log("clearing sessions");
+    if("none" === status) {
         if(this.loggedin) {
             this.loggedin = false;
             this.port.emit("emitevent.logout");
         }
     }
-    else if(sessions.length) {
+    else if("loggedin" === status) {
         // We support cookie login and have sessions.  Keep track of
         // these cookies, we may have to logout.
         this.loggedin = true;
         panel.port.emit("sessions.set", sessions);
-        var username = '';
-        sessions.forEach(function(session) {
-            var boundTo = session.bound_to;
-            if(boundTo && boundTo.type === "cookie") {
-                console.log("bound to a cookie: " + boundTo.name);
-                tab.cookie = boundTo.name;
-                cookieMonster.watch(host, boundTo.name, function() {
-                    //console.log("cookie changed, we should do something");
-                });
-            }
-            username = session.email;
-        });
         console.log("setting loggedIn");
-        tab.loginShown = true;
-        loginWidget.loggedIn(username, host);
     }
-    else if(this.loggedin) {
-        // in this case, sessions is set to [], which means the site
-        // supports our login scheme.  We were logged in, so we have
-        // to logout.
-        this.loggedin = false;
-        this.port.emit("emitevent.logout");
-        console.log("setting login");
-        loginWidget.login(host);                            
-        tab.loginShown = true;
-    }
-    else {
-        // in this case, we have a sessions, but we were not previously
-        // logged in, show the login button only.
-        console.log("setting login");
-        loginWidget.login(host);                            
-        tab.loginShown = true;
+    else if("login" === status) {
     }
 }
 
@@ -106,27 +79,21 @@ function onSessionTabOpen(tab) {
     console.log("session tab open");
 
     console.log("typeof tab.window: " + typeof tab.window);
-    tab.loginShown = false;
 };
 
-/*
+
 tabs.on("open", function(tab) {
   console.log("tab open");
-  tab.expando = "my expando";
+  tab.window.expando = "my expando";
 });
-*/
+
 tabs.on("ready", onSessionTabReady);
 tabs.on("activate", function(tab) {
     console.log("tab activated");
-    console.log("typeof tab.expando: " + typeof tab.expando);
-    console.log("tab.expando: " + tab.expando);
 });
 
 function onSessionTabReady(tab) {
-    if(!tab.expando) {
-      tab.expando = "my expando";
-    }
-    console.log('typeof tab.window: ' + typeof tab.window);
+    console.log("typeof tab.window: " + typeof tab.window);
     if(tab.cookie) {
         console.log("some cookies watched for this tab");
     }
@@ -134,7 +101,7 @@ function onSessionTabReady(tab) {
     timers.setTimeout(function() {
         if(!tab.loginShown) {
             console.log("hiding login");
-            loginWidget.none();
+            //loginWidget.none();
         }
     }, 1000);
 }
@@ -156,7 +123,7 @@ function logoutCallback() {
 };
 
 function sessionCallback() {
-    var el = Helpers.chrome.getElementById('identity-session-box');
+    var el = Helpers.chrome.getElementById("identity-session-box");
     panel.show(el);
 };
 
