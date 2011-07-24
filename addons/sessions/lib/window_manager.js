bcd7369a4101c112b8c2ef932dd4b07dceda30e9
Shane Tomlinson <set117@yahoo.com>
2011-07-23 18:00:18 -0700

    Bringing back the stylesheet attachment.

--- addons/sessions/lib/window_manager.js
+++ addons/sessions/lib/window_manager.js
@@ -1,17 +1,23 @@
+"use strict";
+
 const {WindowTracker} = require("window-utils");
 const {Helpers} = require("./helpers");
 const self = require("self");
 const {Session} = require("session");
 const {SessionDisplay} = require("session_display");
 
+// Before you mess this up again, use the low level windowTracker so that
+// you can get the document.  You need the document to create the CSS and 
+// to attach the sessionDisplay.
+
 let delegate = {
     onTrack: function(window) {
          let doc = window.document;
-/*         let uri = self.data.url("styles/identity-session.css");
+         let uri = self.data.url("styles/identity-session.css");
          Helpers.chrome.loadStylesheet(uri, doc);
-*/
 
-         let windowSession = new Session();
+
+/*         let windowSession = new Session();
          let sessionDisplay = new SessionDisplay({
             document: doc,
             session: windowSession
@@ -19,7 +25,7 @@ let delegate = {
 
          window.session = windowSession;
          console.log("ontrack");
-    },
+  */  },
 
     onUntrack: function(window) {
     }
--- addons/sessions/test/test-window_manager.js
+++ addons/sessions/test/test-window_manager.js
@@ -1,24 +1,45 @@
-let {WindowManager} = require("window_manager");
-let windows = require("windows").browserWindows;
+const {WindowManager} = require("window_manager");
+const {WindowTracker} = require("window-utils");
+const windows = require("windows").browserWindows;
 
-let wm;
+let wm, openCallback, closeCallback;
 exports.setup = function() {
     wm = new WindowManager();
+
+    openCallback = closeCallback = undefined;
+    let tracker = new WindowTracker({
+        onTrack: function(window) {
+            if(openCallback) openCallback(window);
+        },
+        onUntrack: function(window) {
+            if(closeCallback) closeCallback(window);
+        }
+    });
 };
 
 exports['we create it'] = function(test) {
     test.assertEqual(!!wm, true, 'we have a window manager');
 };
 
-/*exports['when opening a window, we get a session'] = function(test) {
+/*exports['each initially open window has a window manager'] = function(test) {
+  let success = true;
+  for each(let window in windows) {
+      success = success && !!window.wm;
+  }
+
+  test.assertStrictEqual(success, true, "all windows have a window manager");
+};
+
+/*
+exports['when opening a window, we get a session'] = function(test) {
     let success = false;
 
-    windows.on("open", function(window) {
-        console.log("window.open");
+    openCallback = function(window) {
         test.assertEqual(!!window.session, true, "a session is put on the browser window");
         test.done();
-    });
+    };
 
     windows.open("http://www.google.com/");
     test.waitUntilDone();
-};*/
+};
+*/
