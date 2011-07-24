8ca222abac7a21ba85853fb3eeb86346e98492f8
Shane Tomlinson <set117@yahoo.com>
2011-07-24 02:28:04 -0700

    Sessions can be undefined/null now

--- addons/sessions/data/sessions.js
+++ addons/sessions/data/sessions.js
@@ -10,8 +10,11 @@
     };
 
     function onSessionsSet(sessions) {
-        username.innerHTML = sessions[0].email;
-        self.port.emit("sessions.setheight", body.clientHeight);
+        onSessionsClear();
+        if(sessions) {
+            username.innerHTML = sessions[0].email;
+            self.port.emit("sessions.setheight", body.clientHeight);
+        }
     }
 
     var logout = document.getElementById("logout");
