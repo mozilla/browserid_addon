c6254e8039ff579c63f48a86ba16f1495b9fa99a
Shane Tomlinson <set117@yahoo.com>
2011-07-24 22:24:41 -0700

    Listen for the login/logout messages and take action.

--- addons/sessions/lib/main.js
+++ addons/sessions/lib/main.js
@@ -6,9 +6,12 @@ const {Fakers} = require("./fakers");
 const {Helpers} = require("./helpers");
 const {WindowManager} = require("window_manager");
 const {TabManager} = require("tab_manager");
+const tabs = require("tabs");
 Fakers();
 
 let windowManager = new WindowManager();
+windowManager.on("login", onLogin);
+windowManager.on("logout", onLogout);
 let tabManager = new TabManager();
 
 let currWorker;
@@ -20,6 +23,7 @@ let pageMod = PageMod({
     onAttach: function(worker) {
         Helpers.workers.prepare(worker);
 
+        worker.tab.worker = worker;
         worker.port.on("sessions.set", onSessionSet.bind(worker));
         worker.port.on("sessions.opentab", onSessionTabOpen.bind(worker));
     }
@@ -39,20 +43,23 @@ function onSessionTabOpen() {
 
 
 
-function loginCallback() {
-    if(currWorker) {
-        currWorker.port.emit("emitevent.login");
+function onLogin() {
+    console.log("received login here");
+    let tab = tabs.activeTab;
+    if(tab) {
+        tab.worker.port.emit("emitevent.login");
     }
 };
 
-function logoutCallback() {
-    if(currWorker) {
-        currWorker.port.emit("emitevent.logout");
+function onLogout() {
+    let tab = tabs.activeTab;
+    if(tab) {
+        tab.worker.port.emit("emitevent.logout");
     }
 };
 
-function sessionCallback() {
-    panel.show(el);
+function onSession() {
+
 };
 
 
