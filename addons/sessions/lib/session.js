11fd8832b9ea978d794ee4a30184b7495c68340f
Shane Tomlinson <stomlinson@mozilla.com>
2011-08-01 13:02:27 -0700

    Cleaning up the add/remove of the bindings.

--- addons/sessions/lib/session.js
+++ addons/sessions/lib/session.js
@@ -7,8 +7,8 @@ let Session = function(config) {
 
     session.getActive = getActive;
     session.noInfo = noInfo;
-
     session.bound = false;
+
     if(config) {
       session.host = config.host;
       session.cookieManager = config.cookieManager;
@@ -43,29 +43,20 @@ function getActive() {
 }
 
 function noInfo() {
-  if (!this.bound) {
+  if (!this.binding) {
     this.sessions = undefined;
   }
 }
 
 function onBeforeSetSessions(sessions) {
-  let active = this.getActive();
-  let binding = active && active.bound_to;
-
-  if(binding && binding.type === "cookie" && this.cookieManager) {
-    this.cookieManager.unwatch(this.host, binding.name, this.onCookieChange);
-    this.bound = false;
-  }
+  removeBinding.call(this);
 }
 
 function onSetSessions(sessions) {
   let active = this.getActive();
   let binding = active && active.bound_to;
 
-  if (binding && binding.type === "cookie" && this.cookieManager) {
-    this.cookieManager.watch(this.host, binding.name, this.onCookieChange);
-    this.bound = true;
-  }
+  addBinding.call(this, binding);
 }
 
 function onBeforeSetHost(host) {
@@ -75,7 +66,26 @@ function onBeforeSetHost(host) {
 }
 
 function onCookieChange() {
-    this.bound = false;
+  removeBinding.call(this);
+}
+
+function addBinding(binding) {
+  if (binding && binding.type === "cookie" && this.cookieManager) {
+    this.binding = {
+      host: this.host,
+      name: binding.name,
+      type: binding.type
+    };
+    this.cookieManager.watch(this.host, binding.name, this.onCookieChange);
+  }
+}
+
+function removeBinding() {
+  var binding = this.binding;
+  if(binding && binding.type === "cookie" && this.cookieManager) {
+    this.cookieManager.unwatch(binding.host, binding.name, this.onCookieChange);
+    this.binding = undefined;
+  }
 }
 
 exports.Session = Session;
