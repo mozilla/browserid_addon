80df60e45748aac603c34b0ac7391177d3121dbf
Shane Tomlinson <stomlinson@mozilla.com>
2011-08-02 15:22:39 -0700

    Woot!  We have arrowage.

--- addons/sessions/lib/session_display.js
+++ addons/sessions/lib/session_display.js
@@ -17,6 +17,7 @@
 
 const {Cc, Ci, Cs, Cr} = require("chrome");
 const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+const SVG_NS = "http://www.w3.org/2000/svg";
 const {EventEmitter} = require("events");
 const STATUS_SHOW = ["loggedin","login"];
 
@@ -30,6 +31,12 @@ const SessionDisplay = EventEmitter.compose({
 
 
         this.hide();
+    },
+
+    teardown: function() {
+        if(this.box) {
+            this.box.parentNode.removeChild();
+        }
     },
         
     show: function() {
@@ -65,7 +72,68 @@ function createUI(document) {
     });
     this.userInfo.addEventListener("click", this._emit.bind(this, "userinfo"), false);
 
-    let insertBefore = document.getElementById("notification-popup-box");
+    this.svg = createNode(document, "svg", {
+        height: "18px",
+        width: "10px",
+        viewBox: "0 0 20 20",
+        preserveAspectRatio: "none",
+        version: "1.1",
+        baseProfile: "full",
+        parentNode: this.box
+    }, SVG_NS);
+
+    let defs = createNode(document, "defs", {
+        parentNode: this.svg
+    }, SVG_NS);
+
+    let gradient = createNode(document, "linearGradient", {
+        id: "sessionArrowGradient",
+        parentNode: defs,
+        x1: "100%",
+        y1: "100%"
+    }, SVG_NS);
+
+    let start = createNode(document, "stop", {
+        offset: "0%",
+        "stop-color": "#cccccc",
+        parentNode: gradient
+    }, SVG_NS);
+
+    let start = createNode(document, "stop", {
+        offset: "100%",
+        "stop-color": "#ffffff",
+        parentNode: gradient
+    }, SVG_NS);
+
+    let arrow = createNode(document, "polygon", {
+      points: "0,1 19,10 0,19",
+      fill: "url(#sessionArrowGradient)",
+      parentNode: this.svg,
+    }, SVG_NS);
+
+    let arrowBorderTop = createNode(document, "line", {
+      x1: "0",
+      y1: "1",
+      x2: "19",
+      y2: "10",
+      stroke: "#cccccc",
+      "stroke-width": "1px",
+      parentNode: this.svg,
+
+    }, SVG_NS);
+
+    let arrowBorderBottom = createNode(document, "line", {
+      x1: "0",
+      y1: "19",
+      x2: "19",
+      y2: "10",
+      stroke: "#cccccc",
+      "stroke-width": "1px",
+      parentNode: this.svg,
+
+    }, SVG_NS);
+
+    let insertBefore = document.getElementById("identity-box");
     if(insertBefore) {
         insertBefore.parentNode.insertBefore(this.box, insertBefore);
     }
@@ -116,8 +184,8 @@ function setStatus(status) {
 
 
 const NODE_SPECIAL = ["parentNode"];
-function createNode(document, nodeName, attribs) {
-     let node = document.createElementNS(XUL_NS, nodeName);
+function createNode(document, nodeName, attribs, ns) {
+     let node = document.createElementNS(ns || XUL_NS, nodeName);
 
      node.show = function() {
         node.collapsed = node.hidden = false;
@@ -133,7 +201,7 @@ function createNode(document, nodeName, attribs) {
             node.setAttribute(attrib, val);
          }
          else if(attrib === "parentNode") {
-             val.insertBefore(node, null);
+             val.appendChild(node);
          }
      }
 
