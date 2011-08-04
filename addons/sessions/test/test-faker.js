b31d25f8e39259b6aa1b4ad8c98b0889727bcc58
Shane Tomlinson <stomlinson@mozilla.com>
2011-08-04 09:53:06 -0700

    Teardown the faker after every request, use a test faker instead of the google faker

--- addons/sessions/test/test-faker.js
+++ addons/sessions/test/test-faker.js
@@ -3,22 +3,24 @@ const {Helpers} = require("helpers");
 
 var faker;
 
+exports.teardown = function() {
+    faker.teardown();
+};
+
 exports['can create with single page'] = function(test) {
-    faker = Faker("*.google.com", "faker_google.js");
+    faker = Faker("*.google.com", "faker_test.js");
 
     test.assertNotUndefined(faker, 'a faker has been created');
 
     var include = faker.include;
     test.assertEqual(1, include.length, 'includes has 1 item');
-    faker.teardown();
 };
 
 exports['can create with multiple matches'] = function(test) {
-    faker = Faker(["*.google.com", "http://google.com/*"], "faker_google.js");
+    faker = Faker(["*.google.com", "http://google.com/*"], "faker_test.js");
 
     var include = faker.include;
     test.assertEqual(2, include.length, 'includes has 2 item');
-    faker.teardown();
 };
 
 
