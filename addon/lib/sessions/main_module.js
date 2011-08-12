519f92cfef7a9a355895cc7eaf3fb96c9208611f
Shane Tomlinson <stomlinson@mozilla.com>
2011-08-11 18:19:19 -0700

    Adding loads of docs about general flow.

--- addon/lib/sessions/main_module.js
+++ addon/lib/sessions/main_module.js
@@ -8,6 +8,31 @@ const tabs = require("tabs");
 const {Permissions} = require("sessions/permissions");
 const timer = require("timer");
 
+/**
+ * The main session module.
+ *
+ * The main flow is - WindowManager is a window watcher.  For every window that 
+ * is opened, a Session model, a SessionDisplay, and a SessionPanel are created.
+ *
+ * A single TabManager keeps track of all tabs that are opened across all 
+ * windows. It creates a session for every tab and does the management of those 
+ * sessions. The  TabManager also keeps track of which tab is currently active, every 
+ * time a new tab becomes active, the TabManager tell that tab's window that a new 
+ * tab has become active and that it should display that tab's sessions. 
+ *
+ * The way this works is that there are two sets of content scripts.  One set 
+ * of content scripts is loaded into every page that is opened into the 
+ * browser.  This set of content scripts listens for changes to 
+ * navigator.id.sessions as well as triggers the login and logout events 
+ * whenever there is interaction with the Sign In and Sign Out buttons that are 
+ * located in the browser chrome.  This set of content scripts also notifies us 
+ * whenever a new page is loaded and whenever the page is ready.
+ *
+ * The second set of content scripts deals with the SessionPanel that is loaded 
+ * into each window.  Whenever the user presses "Sign Out" in the SessionPanel, 
+ * we have to tell the site that it needs to log the user out - this is done 
+ * via the 'logout' event.
+ */
 exports.MainSession = function() {
   let pm = new Permissions();
   let windowManager = new WindowManager();
@@ -22,9 +47,21 @@ exports.MainSession = function() {
       contentScriptWhen: "start",
       contentScriptFile: data.url("sessions/page_interaction.js"),
       onAttach: function(worker) {
-          // only do this if we are in a tab - jetpack panels cause
-          // a worker to be created as well, but we don't want to bind
-          // to them.
+          /**
+           * only do this if we are in a tab - jetpack panels cause
+           * a worker to be created as well, but we don't want to bind
+           * to them.
+           *
+           * sessions.set means the page set navigator.id.sessions and we have 
+           * to update the tab's session model.
+           *
+           * sessions.opentab means that a new page was opened in the tab and 
+           * we have to prepare that tab's session model for a possible reset
+           *
+           * sessions.tabready means that the tab's contents are fully ready 
+           * and that we should either remove the session display or update its 
+           * contents.
+           */
           if (worker.tab) {
               worker.tab.worker = worker;
               worker.port.on("sessions.set", onSessionSet.bind(worker));
@@ -35,18 +72,28 @@ exports.MainSession = function() {
   });
 
 
-
+  /**
+   * The page has set its session data, update the tab's session model.
+   */
   function onSessionSet(data) {
       this.tab.worker = this;
       tabManager.sessionUpdate(this.tab, data);
   }
 
 
+  /**
+   * A new page was opened within the tab, notify the tab's session model that 
+   * it may have to reset itself.
+   */
   function onSessionTabOpen(data) {
       this.tab.worker = this;
       tabManager.sessionReset(this.tab, data);
   };
 
+  /**
+   * The tab's content is now ready, either update the session display or 
+   * remove it if there is no session data.
+   */
   function onSessionTabReady() {
       tabManager.tabReady(this.tab);
   };
@@ -54,6 +101,13 @@ exports.MainSession = function() {
   function emitEvent(eventName) {
       let tab = tabs.activeTab;
       if (tab) {
+          /**
+           * This is ghetto fantastic.  For BrowserID to work, it opens a popup 
+           * window.  The popup window is opened programatically, but is 
+           * normally blocked because there is no click within the page.  We 
+           * are giving temporary permissions to open a popup and then we reset 
+           * the permission to its original state.
+           */
           pm.allow("popup");
           tab.worker.port.emit(eventName);
           timer.setTimeout(function() {
