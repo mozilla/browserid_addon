8df8561009b378dff6f6d15780caff08feba7e4e
Shane Tomlinson <stomlinson@mozilla.com>
2011-09-20 04:30:25 +0100

    More docs!

--- addon/lib/sessions/window_session.js
+++ addon/lib/sessions/window_session.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// WindowSession binds the current Session model for the window to the Session 
+// model for a particular tab.  This will copy all of the tab's Session info 
+// over, and then bind a listener to track any changes to that tab's info.
 let WindowSession = function(config) {
     this._session = config.session;
     this.onSet = onSet.bind(this);
@@ -10,14 +13,18 @@ WindowSession.prototype = {
         return this._session;
     },
     set session(session) {
+        // copy all fields over
         session.keys().forEach(function(field) {
             this._session[field] = session[field];
         }, this);
 
+        // If we were already listening to changes on a tab's session info, 
+        // stop listening.
         if(this.currentSession) {
             this.currentSession.removeListener("set", this.onSet);
         }
 
+        // Start listening on the new tab.
         this.currentSession = session;
         session.on("set", this.onSet);
     }
