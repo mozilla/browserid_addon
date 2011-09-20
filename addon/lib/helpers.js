bc73d9b6d789853be45d49c5fecb43d03228808b
Shane Tomlinson <stomlinson@mozilla.com>
2011-09-20 04:16:55 +0100

    Adding removeStylesheet and stylesheetExists functions and tests.

--- addon/lib/helpers.js
+++ addon/lib/helpers.js
@@ -17,24 +17,24 @@ const Helpers = {
             return win;
         },
         
-        getDocument: function(window) {
-            window = window || Helpers.chrome.getMostRecentWindow();
-            return window.document;
+        getDocument: function(win) {
+            win = win || Helpers.chrome.getMostRecentWindow();
+            return win.document;
         },
 
-        getElementById: function(id, document) {
-            document = document || Helpers.chrome.getDocument();                                       
-            let el = document.getElementById(id);
+        getElementById: function(id, doc) {
+            doc = doc || Helpers.chrome.getDocument();                                       
+            let el = doc.getElementById(id);
             return el;
         },
 
 
-        simulateDOMEvent: function(el, evtClass, type, document) {
-            document = document || Helpers.chrome.getDocument();                    
-            let evt = document.createEvent(evtClass);
+        simulateDOMEvent: function(el, evtClass, type, doc) {
+            doc = doc || Helpers.chrome.getDocument();                    
+            let evt = doc.createEvent(evtClass);
 
             if(evtClass === "MouseEvents") {
-                evt.initMouseEvent(type, true, true, document.defaultView,
+                evt.initMouseEvent(type, true, true, doc.defaultView,
                         0, 0, 0, 0, 0, false, false, false, false, 0, null);
             }
 
@@ -43,14 +43,52 @@ const Helpers = {
             return evt;
         },
 
-        loadStylesheet: function(uri, document) {
-            document = document || Helpers.chrome.getDocument();
-            let pi = document.createProcessingInstruction(
-                   "xml-stylesheet", "href=\"" + uri + "\" type=\"text/css\"");
-                document.insertBefore(pi, document.firstChild);
+        loadStylesheet: function(uri, doc) {
+            doc = doc || Helpers.chrome.getDocument();
+            let pi;
+            if (!Helpers.chrome.stylesheetExists(uri, doc)) {
+              pi = doc.createProcessingInstruction(
+                     "xml-stylesheet", getXMLStylesheetInstruction(uri)); 
+              doc.insertBefore(pi, doc.firstChild);
+            }
             return pi;
+        },
+
+        removeStylesheet: function(uri, doc) {
+            doc = doc || Helpers.chrome.getDocument();
+            let sheet = getStylesheet(uri, doc);
+
+            if (sheet) {
+              sheet.parentNode.removeChild(sheet);
+            }
+        },
+
+        stylesheetExists: function(uri, doc) {
+            doc = doc || Helpers.chrome.getDocument();
+            let sheet = getStylesheet(uri, doc);
+
+            return !!sheet;
         }
+
+
     }
 };
+
+function getXMLStylesheetInstruction(uri) {
+  return "href=\"" + uri + "\" type=\"text/css\"";
+}
+
+function getStylesheet(uri, doc) {
+  let children = doc.childNodes;
+
+  for(let len = children.length - 1; len >= 0; --len) {
+    let sheet = children[len];
+    let data = sheet.data;
+    if (sheet.nodeName == "xml-stylesheet" && 
+        data == getXMLStylesheetInstruction(uri)) {
+      return sheet; 
+    }
+  };
+}
 
 exports.Helpers = Helpers;
