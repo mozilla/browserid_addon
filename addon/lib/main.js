d3ce2c8430dafe06759ad98f32551b19d9f291f1
Shane Tomlinson <stomlinson@mozilla.com>
2011-11-02 20:09:13 +0000

    Remove the exports.main function.

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -1,8 +1,6 @@
 const {MainSession} = require("sessions/main_module");
 const {MainBrowserID} = require("browserid/main_module");
 
-exports.main = function() {
-  MainSession();
-  MainBrowserID();
-}
+MainSession();
+MainBrowserID();
 
